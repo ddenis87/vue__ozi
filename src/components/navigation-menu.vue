6ce9733243fd701e0309dcc290b537f4d93c17b8
ddenis87 <dizdok@yandex.ru>
2020-09-22 14:48:25 +0900

    fix

--- src/components/navigation-menu.vue
+++ src/components/navigation-menu.vue
@@ -95,13 +95,14 @@ export default {
         border-bottom: 1px solid grey;
         cursor: pointer;
         transition: padding-left 0.3s;
+
       }
       
       &__items:last-child { border: 0px; }
       &__items:hover { text-decoration: none; color: black; padding-left: 20px;}
-      &__items_title { padding-left: 3px; font-weight: bold; cursor: default; }
+      &__items_title { padding-left: 3px; font-weight: bold; cursor: default; font-size: 14px;}
       &__items_title:hover { padding-left: 3px; }
-      &__link { text-decoration: none; color: black; }
+      &__link { text-decoration: none; color: black; font-size: 14px;}
       
     }
   }
