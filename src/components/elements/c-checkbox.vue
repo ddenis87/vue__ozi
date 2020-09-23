d739cfcb0e383bb25d435b6264a34fae1210b54c
ddenis87 <dizdok@yandex.ru>
2020-09-23 15:19:01 +0900

    add elements c-input, c-checkbox

---
+++ src/components/elements/c-checkbox.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="c-checkbox">
+    <label class="c-checkbox__title">
+      <input class="c-checkbox__checkbox" type="checkbox"/>
+      <div class="c-checkbox__slider">
+        <div class="c-checkbox__slider-item"></div>
+      </div>
+      <slot></slot>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cCheckbox'  
+}
+</script>
+
+<style lang="scss" scoped>
+.c-checkbox {
+  &__title {
+    display: inline-flex;
+    align-items: center;
+    font-size: 10px;
+    font-weight: bold;
+    text-transform: uppercase;
+    color: rgba(0,0,0, .8);
+  }
+  display: inline-flex;
+  align-items: center;
+  &__checkbox {
+    position: absolute;
+    opacity: 0;
+    z-index: -1;
+    & + .c-checkbox__slider {
+      display: flex;
+      align-items: center;
+      margin-right: 5px;
+      padding: 2px 4px;
+      width: 40px;
+      height: 20px;
+      border: 1px solid grey;
+      border-radius: 10px;
+      box-sizing: border-box;
+      transition: 0.4s;
+      .c-checkbox__slider-item {
+        width: 12px;
+        height: 12px;
+        border: 1px solid grey;
+        border-radius: 8px;
+        background-color: grey;
+        box-sizing: border-box;
+      }
+    }
+    &:checked {
+      & + .c-checkbox__slider {
+        padding-left: 22px;
+        .c-checkbox__slider-item {
+          border: 1px solid rgb(54, 96, 146);
+          background-color:  rgb(54, 96, 146);
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/elements/c-input.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="c-input">
+    <label class="c-input__title"><slot></slot></label>
+    <input class="c-input__input" 
+           type="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cInput',  
+}
+</script>
+
+<style lang="scss" scoped>
+.c-input {
+  position: relative;
+  &__title {
+    position: absolute;
+    left: 5px;
+    top: 5px;
+    font-size: 8px;
+    text-transform: uppercase;
+  }
+  &__input {
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    padding: 3px;
+    padding-left: 5px;
+    padding-top: 16px;
+    font-size: 14px;
+    border: 1px solid grey;
+    border-radius: 3px;
+    box-sizing: border-box;
+  }
+}
+</style>
