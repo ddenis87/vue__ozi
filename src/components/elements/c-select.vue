d54caa3a1d9e734bddab084320707a7dd9fcdc4a
ddenis87 <dizdok@yandex.ru>
2020-09-29 09:56:24 +0900

    Add v-model and emit c-select

--- src/components/elements/c-select.vue
+++ src/components/elements/c-select.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="c-select">
     <label class="c-select__title"><slot></slot></label>
-    <select class="c-select__select">
+    <select class="c-select__select"
+            v-model="selectValue"
+            @change="changeSelectValue">
       <option v-for="(item, index) in listItem" 
               :key="index" 
-              :value="item.ID">{{ item.CNAME }}</option>
+              :value="item">{{ item.CNAME }}</option>
     </select>
   </div>  
 </template>
@@ -12,14 +14,24 @@
 <script>
 export default {
   name: 'cSelect',
+  model: {
+    event: 'change'
+  },
   props: [
     'inListItem',
   ],
   computed: {
     listItem() { return this.inListItem }
   },
-  created: function() {
-    
+  data: function() {
+    return {
+      selectValue: '0',
+    }
+  },
+  methods: {
+    changeSelectValue: function() {
+      this.$emit('change', this.selectValue);
+    }
   }
 }
 </script>
