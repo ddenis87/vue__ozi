6f1f5b3b0631cd509a85d81ed38501e67586deea
ddenis87 <dizdok@yandex.ru>
2020-09-29 13:14:00 +0900

    Add personProfile in store state

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,26 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    userProfile: {
+    userProfile: { // пользователь, кто работает
       userId: '',
       userIp: '', // ?
       userNameFull: '',
       userNameShort: '',
       userAccess: '',
     },
+    personProfile: { // пользователь с которым работают
+      personId: '',
+      personFa: '',
+      personIm: '',
+      personOt: '',
+      personNameFull: '',
+      personDistrictId: '',
+      personDistrictName: '',
+      personDepartmentId: '',
+      personDepartmentName: '',
+      personPostId: '',
+      personPostName: '',
+    }
     // userProfile: {
     //   userId: '3',
     //   userIp: '10.38.0.112', // ?
@@ -32,6 +45,18 @@ export default new Vuex.Store({
       state.userProfile.userNameFull = option.userNameFull;
       state.userProfile.userNameShort = option.userNameShort;
       state.userProfile.userLevelAccess = option.userLevelAccess;
+    },
+    setPersonProfile(state, option) {
+      state.personProfile.personId = option.VID;
+      state.personProfile.personFa = option.VFA;
+      state.personProfile.personIm = option.VIM;
+      state.personProfile.personOt = option.VOT;
+      state.personProfile.personDistrictId = option.VDISTRICTID;
+      state.personProfile.personDistrictName = option.VDISTRICTNAME;
+      state.personProfile.personDepartmentId = option.VDEPARTMENTID;
+      state.personProfile.personDepartmentName = option.VDEPARTMENTNAME;
+      state.personProfile.personPostId = option.VPOSTID;
+      state.personProfile.personPostName = option.VPOSTNAME;
     }
     // systemoutState(state, systemoutProps) { 
     //   state.systemout.text = systemoutProps.text; 
