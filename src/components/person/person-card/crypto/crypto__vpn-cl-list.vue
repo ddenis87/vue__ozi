43eb270aae5e1bca7f90ffc1f1091967633c117f
ddenis87 <dizdok@yandex.ru>
2020-11-05 15:02:11 +0900

    Rebuild style crypto__vpn-list

--- src/components/person/person-card/crypto/crypto__vpn-cl-list.vue
+++ src/components/person/person-card/crypto/crypto__vpn-cl-list.vue
@@ -1,60 +1,58 @@
 <template>
-  <div class="crypto-list">
-    <table class="crypto-table">
-      <caption class="crypto-table__title"><slot></slot></caption>
-      <thead class="crypto-table__head">
-        <tr class="crypto-table__head_row">
-          <th rowspan="2" class="crypto-table__head_column">№ п/п</th>
-          <th rowspan="2" class="crypto-table__head_column">Наименование</th>
-          <th colspan="3" class="crypto-table__head_column">Задачи</th>
-          <th rowspan="2" class="crypto-table__head_column">Установка</th>
-          <th rowspan="2" class="crypto-table__head_column">Акты</th>
-          <th rowspan="2" class="crypto-table__head_column">Удаление</th>
-          <th rowspan="2" class="crypto-table__head_column">Акты</th>
-          <th rowspan="2" class="crypto-table__head_column crypto-table__head_column-action"></th>
+  <div class="vpn-list">
+    <table class="vpn-table">
+      <caption class="vpn-table__title"><slot></slot></caption>
+      <thead class="vpn-table__head">
+        <tr class="vpn-table__head-row">
+          <th rowspan="2" class="vpn-table__head-column">№ п/п</th>
+          <th rowspan="2" class="vpn-table__head-column">Наименование</th>
+          <th colspan="3" class="vpn-table__head-column vpn-table__head-column_task">Задачи</th>
+          <th rowspan="2" class="vpn-table__head-column vpn-table__head-column_basis install">Основание установки</th>
+          <th rowspan="2" class="vpn-table__head-column vpn-table__head-column_document install">Акты</th>
+          <th rowspan="2" class="vpn-table__head-column vpn-table__head-column_basis unistall">Основание удаления</th>
+          <th rowspan="2" class="vpn-table__head-column vpn-table__head-column_document unistall">Акты</th>
+          <th rowspan="2" class="vpn-table__head-column vpn-table__head-column_action"></th>
         </tr>
-        <tr class="crypto-table__head_row">
-          <th class="crypto-table__head_column-task">З</th>
-          <th class="crypto-table__head_column-task">ДП</th>
-          <th class="crypto-table__head_column-task">К</th>
+        <tr class="vpn-table__head_row">
+          <th class="vpn-table__head-column vpn-table__head-column_task">З</th>
+          <th class="vpn-table__head-column vpn-table__head-column_task">ДП</th>
+          <th class="vpn-table__head-column vpn-table__head-column_task">К</th>
         </tr>
       </thead>
-      <tbody class="crypto-table__body">
+      <tbody class="vpn-table__body">
         <template v-for="(item, index) in listItem">
-          <tr class="crypto-table__body_row" :key="index + 1">
-            <td class="crypto-table__body_column">{{ index + 1 }}</td>
-            <td class="crypto-table__body_column">{{ item.VNAME }}</td>
-            <td class="crypto-table__body_column crypto-table__body_column-task" 
-                :class="{'crypto-table__body_column-task_yes': (item.VTASKCLIENT == 1) ? true : false }" 
+          <tr class="vpn-table__body-row" :key="index + 1">
+            <td class="vpn-table__body-column">{{ index + 1 }}</td>
+            <td class="vpn-table__body-column">{{ item.VNAME }}</td>
+            <td class="vpn-table__body-column vpn-table__body-column_task task" 
+                :class="{'task_yes': (item.VTASKCLIENT == 1) ? true : false }" 
                 @click="(event) => openDialogTask(event, item.VID, item.VTASKCLIENT, item.VTASKMAIL, item.VTASKCS )"></td>
-            <td class="crypto-table__body_column crypto-table__body_column-task" 
-                :class="{'crypto-table__body_column-task_yes': (item.VTASKMAIL == 1) ? true : false }" 
+            <td class="vpn-table__body-column vpn-table__body-column_task task" 
+                :class="{'task_yes': (item.VTASKMAIL == 1) ? true : false }" 
                 @click="(event) => openDialogTask(event, item.VID, item.VTASKCLIENT, item.VTASKMAIL, item.VTASKCS)"></td>
-            <td class="crypto-table__body_column crypto-table__body_column-task" 
-                :class="{'crypto-table__body_column-task_yes': (item.VTASKCS == 1) ? true : false }" 
+            <td class="vpn-table__body-column vpn-table__body-column_task task" 
+                :class="{'task_yes': (item.VTASKCS == 1) ? true : false }" 
                 @click="(event) => openDialogTask(event, item.VID, item.VTASKCLIENT, item.VTASKMAIL, item.VTASKCS)"></td>
-            <td class="crypto-table__body_column crypto-table__body_column-base">
+            <td class="vpn-table__body-column install">
               <list-control-basis :basisProps="{ basisText: item.VINSTALLDOCNUMBER, basisDate: item.VINSTALLDOCDATE }"
                                   @open-dialog-basis="openDialogBasis(item.VID, item.VINSTALLDOCID, 'install')"></list-control-basis>
             </td>
-            <td class="crypto-table__body_column">
+            <td class="vpn-table__body-column install">
               <list-control-file :upload="false"></list-control-file>
             </td>
-            <td class="crypto-table__body_column crypto-table__body_column-base">
+            <td class="vpn-table__body-column unistall">
               <list-control-basis :basisProps="{ basisText: item.VUNISTALLDOCNUMBER, basisDate: item.VUNISTALLDOCDATE }"
                                   @open-dialog-basis="openDialogBasis(item.VID, item.VUNISTALLDOCID, 'unistall')"></list-control-basis>
             </td>
-            <td class="crypto-table__body_column">
+            <td class="vpn-table__body-column unistall">
               <list-control-file :upload="false"></list-control-file>
             </td>
-            <td rowspan="2" class="crypto-table__body_column">
-              <div class="control">
-                <button class="control__button control__button_delete-row" title="Удалить запись"></button>
-              </div>
+            <td rowspan="2" class="vpn-table__body-column">
+                <button class="button_delete" title="Удалить запись"></button>
             </td>
           </tr>
-          <tr class="crypto-table__body_row" :key="index + 1 * 1000">
-            <td colspan="9" class="crypto-table__body_column crypto-table__body_column-end">
+          <tr class="vpn-table__body_row" :key="index + 1 * 1000">
+            <td colspan="9" class="vpn-table__body-column vpn-table__body-column_note">
               <div class="note">
                 <div class="note__item"><b>Примечание установки: </b>{{ (item.VINSTALLNOTE) ? item.VINSTALLNOTE : 'Отсутствует' }}</div>
                 <div class="note__item"><b>Примечание удаления: </b>{{ (item.VUNISTALLNOTE) ? item.VUNISTALLNOTE : 'Отсутствует' }}</div>
@@ -64,7 +62,7 @@
         </template>
       </tbody>
     </table>
-    <div class="crypto-dialog">
+    <div class="vpn-dialog">
       <dialog-task v-if="dialogPropsTask.valueVisibility" 
                    :style="{left: dialogPropsTask.posLeft + 'px', top: dialogPropsTask.posTop + 'px'}"
                    :dialogProps="dialogPropsTask"
@@ -75,7 +73,7 @@
                     @cancel-update="() => { dialogPropsBasis.valueVisibility = false }"
                     @update-basis="updateBasis">Документ основание</dialog-basis>
     </div>
-    <div class="crypto__blocked-content"
+    <div class="vpn__blocked-content"
          v-if="(dialogPropsTask.valueVisibility || dialogPropsBasis.valueVisibility)"></div>
   </div>
 </template>
@@ -134,89 +132,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.crypto {
-  &-list {
-    font-family: 'Open sans';
-    font-size: 13px;
-  }
-  &-table {
-    width: 100%;
-    border-collapse: collapse;
-    &__title {
-      text-align: left;
-      font-family: 'Montserrat';
-      font-size: 14px;
-      font-weight: bold;
-      border-bottom: 2px solid grey;
-    }
+@import 'vpn-list.scss';
+
+.vpn-list {
+  .vpn-table {
     &__head {
-      &_column {
-        padding: 3px;
-        border-bottom: 2px solid grey;
-        &:nth-child(1) { width: 42px; }
-        &:nth-child(3) { border-left: 2px solid grey; border-right: 2px solid grey; }
-        &:nth-child(5), &:nth-child(7) { width: 70px; border-right: 2px solid darkgray; border-left: 1px solid darkgray; }
-        &:nth-child(4), &:nth-child(6) { width: 230px; }
-        &:nth-child(4), &:nth-child(5) { background-color: lightblue; }
-        &:nth-child(6), &:nth-child(7) { background-color: lightgreen; }
-        &:nth-child(8) {
-          width: 30px;
-          background-image: url('~@/assets/images/view/action.png');
-          background-size: 18px 18px;
-          background-repeat: no-repeat;
-          background-position: center;
-        }
-        &-task {
-          padding: 3px;
-          border-bottom: 1px solid grey;
-          width: 25px;
-          text-align: center; 
-          font-size: 10px; 
-          font-weight: bold; 
-          &:nth-child(1) { border-left: 2px solid grey; border-bottom: 2px solid grey; }
-          &:nth-child(2) { border-left: 1px solid grey; border-right: 1px solid grey; border-bottom: 2px solid grey; }
-          &:nth-child(3) { border-right: 2px solid grey; border-bottom: 2px solid grey; }
+      &-column {
+        &_task {
+          border-left: 2px solid grey; border-right: 2px solid grey;
+          &:nth-child(1), &:nth-child(2), &:nth-child(3) {
+            text-align: center;
+            width: 30px;
+          }
         }
       }
     }
     &__body {
-      &_column {
-        padding: 5px 5px;
-        &-end {
-          border-bottom: 1px solid grey;
-          .note {
-            display: flex;
-            justify-content: space-between;
-          }
-        }
-        &:nth-child(7), &:nth-child(9) { border-right: 2px solid darkgray; border-left: 1px solid darkgray; }
-        &:nth-child(6), &:nth-child(7) { background-color: rgba(173, 216, 230, .3); }
-        &:nth-child(8), &:nth-child(9) { background-color: rgba(144, 238, 144, .3); }
-        &:nth-child(10) { background-color: #FF9200; }
-        &-base { width: 250px; }
-        &-task {
-          text-align: center;
-          background-image: url('~@/assets/images/view/no.png');
-          background-size: 14px 14px;
-          background-repeat: no-repeat;
-          background-position: center;
-          cursor: pointer;
-          &_yes { background-image: url('~@/assets/images/view/yes.png'); }
-        }
+      .task { 
+        background-image: url('~@/assets/images/view/no.png');
+        background-size: 14px 14px;
+        background-repeat: no-repeat;
+        background-position: center;
+        cursor: pointer;
+        &_yes { background-image: url('~@/assets/images/view/yes.png'); }
       }
     }
-  }
-  &-dialog {
-    position: relative;
-    z-index: 998;
-  }
-  &__blocked-content {
-    position: fixed;
-    left: 0px;
-    top: 0px;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, .3);
   }
 }
 </style>
--- src/components/person/person-card/crypto/crypto__vpn-cs-list.vue
+++ src/components/person/person-card/crypto/crypto__vpn-cs-list.vue
@@ -2,44 +2,42 @@
   <div class="vpn-list">
     <table class="vpn-table">
       <caption class="vpn-table__title"><slot></slot></caption>
-      <thead class="vpn-table-cs__head">
-        <tr class="vpn-table-cs__head_row">
-          <th class="vpn-table-cs__head_column">№ п/п</th>
-          <th class="vpn-table-cs__head_column">Наименование</th>
-          <th class="vpn-table-cs__head_column vpn-table-cs__head_column-basis install">Основание установки</th>
-          <th class="vpn-table-cs__head_column vpn-table-cs__head_column-document install">Акты</th>
-          <th class="vpn-table-cs__head_column vpn-table-cs__head_column-basis unistall">Основание удаления</th>
-          <th class="vpn-table-cs__head_column vpn-table-cs__head_column-document unistall">Акты</th>
-          <th class="vpn-table-cs__head_column crypto-table__head_column-action"></th>
+      <thead class="vpn-table__head">
+        <tr class="vpn-table__head-row">
+          <th class="vpn-table__head-column">№ п/п</th>
+          <th class="vpn-table__head-column">Наименование</th>
+          <th class="vpn-table__head-column vpn-table__head-column_basis install">Основание установки</th>
+          <th class="vpn-table__head-column vpn-table__head-column_document install">Акты</th>
+          <th class="vpn-table__head-column vpn-table__head-column_basis unistall">Основание удаления</th>
+          <th class="vpn-table__head-column vpn-table__head-column_document unistall">Акты</th>
+          <th class="vpn-table__head-column vpn-table__head-column_action"></th>
         </tr>
       </thead>
-      <tbody class="vpn-table-cs__body">
+      <tbody class="vpn-table__body">
         <template v-for="(item, index) in listItem">
-          <tr class="vpn-table-cs__body_row" :key="index + 1">
-            <td class="vpn-table-cs__body_column">{{ index + 1 }}</td>
-            <td class="vpn-table-cs__body_column">{{ item.VNAME }}</td>
-            <td class="vpn-table-cs__body_column vpn-table-cs__body_column-basis install">
+          <tr class="vpn-table__body-row" :key="index + 1">
+            <td class="vpn-table__body-column">{{ index + 1 }}</td>
+            <td class="vpn-table__body-column">{{ item.VNAME }}</td>
+            <td class="vpn-table__body-column vpn-table__body-column_basis install">
               <list-control-basis :basisProps="{ basisText: item.VINSTALLDOCNUMBER, basisDate: item.VINSTALLDOCDATE }"
                                   @open-dialog-basis="openDialogBasis(item.VID, item.VINSTALLDOCID, 'install')"></list-control-basis>
             </td>
-            <td class="vpn-table-cs__body_column vpn-table-cs__body_column-document install">
+            <td class="vpn-table__body-column vpn-table__body-column_document install">
               <list-control-file :upload="false"></list-control-file>
             </td>
-            <td class="vpn-table-cs__body_column vpn-table-cs__body_column-basis unistall">
+            <td class="vpn-table__body-column vpn-table__body-column_basis unistall">
               <list-control-basis :basisProps="{ basisText: item.VUNISTALLDOCNUMBER, basisDate: item.VUNISTALLDOCDATE }"
                                   @open-dialog-basis="openDialogBasis(item.VID, item.VUNISTALLDOCID, 'unistall')"></list-control-basis>
             </td>
-            <td class="vpn-table-cs__body_column vpn-table-cs__body_column-document unistall">
+            <td class="vpn-table__body-column vpn-table__body-column_document unistall">
               <list-control-file :upload="true"></list-control-file>
             </td>
-            <td class="vpn-table-cs__body_column">
-              <div class="control">
-                <button class="control__button control__button_delete-row" title="Удалить запись"></button>
-              </div>
+            <td rowspan="2" class="vpn-table__body-column">
+                <button class="button_delete" title="Удалить запись"></button>
             </td>
           </tr>
-          <tr class="vpn-table-cs__body_row" :key="index + 1 * 1000">
-            <td colspan="9" class="vpn-table-cs__ody_column vpn-table-cs__body_column-end">
+          <tr class="vpn-table__body-row" :key="index + 1 * 1000">
+            <td colspan="6" class="vpn-table__body-column vpn-table__body-column_note">
               <div class="note">
                 <div class="note__item"><b>Примечание установки: </b>{{ (item.VINSTALLNOTE) ? item.VINSTALLNOTE : 'Отсутствует' }}</div>
                 <div class="note__item"><b>Примечание удаления: </b>{{ (item.VUNISTALLNOTE) ? item.VUNISTALLNOTE : 'Отсутствует' }}</div>
@@ -92,50 +90,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.vpn-list {
-    font-family: 'Open sans';
-    font-size: 12px;
-  .vpn-table {
-    width: 100%;
-    border-collapse: collapse;
-    &__title {
-      padding: 3px;
-      border-bottom: 2px solid grey;
-      text-align: left;
-      font-family: 'Montserrat';
-      font-size: 1.1em;
-      font-weight: bold;
-    }
-    &-cs__head {
-      &_row {
-        .install { background-color: lightblue; }
-        .unistall { background-color: lightgreen; }
-      }
-      &_column {
-        padding: 3px;
-        border-bottom: 2px solid grey;
-        &:first-child { width: 42px; }
-        &-basis { width: 250px; }
-        &-document { width: 70px; border-right: 2px solid darkgray; border-left: 1px solid darkgray; }
-        &:last-child {
-          width: 30px;
-          background-image: url('~@/assets/images/view/action.png');
-          background-size: 18px 18px;
-          background-repeat: no-repeat;
-          background-position: center;
-        }
-      }
-    }
-    &-cs__body {
-      &_row {
-        .install { background-color: rgba(173, 216, 230, .3); }
-        .unistall { background-color: rgba(144, 238, 144, .3); }
-      }
-      &_column {
-        padding: 3px 3px;
-        &-document { border-right: 2px solid darkgray; border-left: 1px solid darkgray; }
-      }
-    }
-  }
-}
+@import 'vpn-list.scss';
 </style>
