9c65b3483ff9213fd5bca26434dadf17be13e5ee
ddenis87 <dizdok@yandex.ru>
2020-11-05 11:29:25 +0900

    Fixed list control basis

--- src/components/person/person-card/crypto/list-control_basis.vue
+++ src/components/person/person-card/crypto/list-control_basis.vue
@@ -1,60 +1,18 @@
 <template>
   <div class="list-control-basis">
-    <span class="list-control-basis__text">{{ getFormatedDocumentBase((basisProp.basisType == 'install') ? basisProp.VINSTALLDOCNUMBER : basisProp.VUNISTALLDOCNUMBER, (basisProp.basisType == 'install') ? basisProp.VINSTALLDOCDATE : basisProp.VUNISTALLDOCDATE) }}</span>
-    <button class="list-control-basis__button" title="Выбрать" @click="openDialog">...</button>
-    <dialog-basis v-if="dialogProps.valueVisibility"
-                  :dialogProps="dialogProps"
-                  @basis-cancel="() => { dialogProps.valueVisibility = false }"
-                  @basis-update="basisUpdate">Документ основание</dialog-basis>
+    <span class="list-control-basis__text">{{ getFormatedDocumentBase(basisProps.basisText, basisProps.basisDate) }}</span>
+    <button class="list-control-basis__button" title="Выбрать" @click="$emit('open-dialog-basis')">...</button>
   </div>
 </template>
 
 <script>
-import DialogBasis from './dialog__basis';
 
 export default {
   name: 'ListControlBasis',
-  components: {
-    DialogBasis
-  },
   props: {
-    basisProp: Object,
-    // basisId: { type: String },
-    // basisDocumentId: { type: String },
-    // basisText: { type: String, default: null },
-    // basisDate: { type: String, default: ''},
-    // basisType: { type: String }
-  },
-  data() {
-    return {
-      dialogProps: {
-        valueVisibility: false,
-        valueId: null,
-        valueDocumentId: null,
-        valueType: null,
-      },
-    }
-  },
-  created() {
-    
+    basisProps: Object,
   },
   methods: {
-    basisUpdate(option) {
-      this.$emit('basis-update', option);
-      this.dialogPropsBasis.visibility = false;
-    },
-    openDialog() {
-      console.log(this.basisProp);
-      this.dialogProps.valueVisibility = true;
-      this.dialogProps.valueId = this.basisProp.VID;
-      this.dialogProps.valueDocumentId = (this.basisProp.basisType == 'install') ? this.basisProp.VINSTALLDOCID : this.basisProp.VUNISTALLDOCID;
-      this.dialogProps.valueType = this.basisProp.basisType;
-      // this.dialogPropsBasis.visibility = true;
-      // this.dialogPropsBasis.documentBasisId = this.basisDocumentId;
-      // this.dialogPropsBasis.listItemId = this.basisId;
-      // this.dialogPropsBasis.typeBasis = this.basisType;
-      
-    },
     getFormatedDocumentBase(basisText, basisDate) {
       if (basisText == null) return '---';
       return `Заявка № ${basisText} от ${basisDate}`;
